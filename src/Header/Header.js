88202fe67be952b1fca4dc59713d3c74447653d6
Enoch <icoder112@gmail.com>
2020-11-08 12:51:59 -0500

    first commit

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+    </nav>
+  );
+}
+
+export default Header;
